381822a98e5000c0a19e17d94237dce090907e80
Jorge Villegas <jorge.vpolanco@gmail.com>
2023-01-08 22:03:35 -0500

    terminando detalle producto

--- lib/modules/product/page/detalle.dart
+++ lib/modules/product/page/detalle.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/modules/cart/page/carrito_screen.dart';
+import 'package:myapp/modules/cart/services/cart_provider.dart';
 import 'package:myapp/modules/product/services/product_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -13,17 +15,37 @@ class _DettaleScreen extends State<DettaleScreen> {
   @override
   Widget build(BuildContext context) {
     final productoProvider = Provider.of<ProductProvider>(context);
+    final cartProvider = Provider.of<CartProvider>(context);
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Shopping Cart"),
+        title: const Text("Detale Producto"),
         elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {
+              if (cartProvider.carProducts.isEmpty) {
+                const snackBar = SnackBar(
+                  content: Text('No hay productos en el carrito!'),
+                );
+                ScaffoldMessenger.of(context).showSnackBar(snackBar);
+              } else {
+                Navigator.of(context).push(
+                  MaterialPageRoute(
+                      builder: (context) => const CarritoScreen()),
+                );
+              }
+            },
+            icon: const Icon(Icons.shopping_cart),
+          ),
+        ],
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(15),
             child: SizedBox(
-              height: 175,
+              height: 400,
               child: FadeInImage(
                 placeholder: const AssetImage("assets/jar-loading.gif"),
                 image: NetworkImage(
@@ -33,14 +55,50 @@ class _DettaleScreen extends State<DettaleScreen> {
             ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 25),
-            child: Text(productoProvider.producto.nombre),
+            padding: const EdgeInsets.symmetric(
+              vertical: 15,
+              horizontal: 25,
+            ),
+            child: Text(
+              productoProvider.producto.nombre,
+              style: const TextStyle(
+                fontSize: 30,
+              ),
+            ),
           ),
           Padding(
-            padding: const EdgeInsets.symmetric(vertical: 25),
-            child: Text(productoProvider.producto.descripcion),
+            padding: const EdgeInsets.symmetric(
+              vertical: 15,
+              horizontal: 25,
+            ),
+            child: Text(
+              productoProvider.producto.descripcion,
+              style: const TextStyle(
+                fontSize: 20,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            child: Text(
+              "\$ ${productoProvider.producto.precio}",
+              style: const TextStyle(
+                color: Colors.black87,
+                fontSize: 40,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
           ),
         ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        label: Row(
+          children: const [
+            Text("Agregar al carrito"),
+          ],
+        ),
+        onPressed: () {},
       ),
     );
   }

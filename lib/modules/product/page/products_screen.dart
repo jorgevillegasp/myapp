edbec5dfdf98d2e503e0a1e49df80be432fcd1b6
Jorge Villegas <jorge.vpolanco@gmail.com>
2023-01-08 20:33:55 -0500

    mejorado el diseño

--- lib/modules/product/page/products_screen.dart
+++ lib/modules/product/page/products_screen.dart
@@ -47,98 +47,154 @@ class _ProductsScreenState extends State<ProductsScreen> {
           ),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 5),
-        child: GridView.builder(
-            itemCount: productsProvider.products.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 0.65,
+      body: Column(
+        children: [
+          const Padding(
+            
+            padding: EdgeInsets.symmetric(
+              vertical: 0,
             ),
-            itemBuilder: (context, index) => Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Column(
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.circular(15),
-                    child: SizedBox(
-                      height: 175,
-                      child: FadeInImage(
-                        placeholder: const AssetImage("assets/jar-loading.gif"),
-                        image: NetworkImage(
-                            "https://gestion.promo.ec/${productsProvider.products[index].imagenes[0]}"),
-                        fit: BoxFit.cover,
-                      ),
-                    ),
+            child: Text(
+              "Categorias",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 15),
+            child: SizedBox(
+              height: 25,
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: categoriaProvider.categories.length,
+                itemBuilder: (context, index) => Padding(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 20,
+                    vertical: 2,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
+                  child: GestureDetector(
                     child: Text(
-                      productsProvider.products[index].nombre,
+                      categoriaProvider.categories[index].nombre,
                       style: const TextStyle(
-                        color: Colors.black45,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
+                    onTap: () {
+                      setState(() {
+                        categoriaProvider.categoria =
+                            categoriaProvider.categories[index];
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return const ProductsScreen();
+                            },
+                          ),
+                        );
+                      });
+                    },
                   ),
-                  Text(
-                    ("\$ ${productsProvider.products[index].precio}"),
-                    style: const TextStyle(
-                      color: Colors.black87,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              )
-            ],
+                ),
+              ),
+            ),
           ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 5),
+              child: GridView.builder(
+                itemCount: productsProvider.products.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  childAspectRatio: 0.65,
+                ),
+                itemBuilder: (context, index) => Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Column(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(15),
+                          child: SizedBox(
+                            height: 175,
+                            child: FadeInImage(
+                              placeholder:
+                                  const AssetImage("assets/jar-loading.gif"),
+                              image: NetworkImage(
+                                  "https://gestion.promo.ec/${productsProvider.products[index].imagenes[0]}"),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text(
+                            productsProvider.products[index].nombre,
+                            style: const TextStyle(
+                              color: Colors.black45,
+                            ),
+                          ),
+                        ),
+                        Text(
+                          ("\$ ${productsProvider.products[index].precio}"),
+                          style: const TextStyle(
+                            color: Colors.black87,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
           ),
+        ],
       ),
 
-        // child: Column(
-        //   crossAxisAlignment: CrossAxisAlignment.start,
-        //   children: [
-        //     Column(
-        //       children: [
-        //         Container(
-        //           padding: const EdgeInsets.all(10),
-        //           height: 200,
-        //           width: 180,
-        //           decoration: BoxDecoration(
-        //               color: Colors.amber,
-        //               borderRadius: BorderRadius.circular(15)),
-        //           child: SizedBox(
-        //             child: FadeInImage(
-        //               placeholder: const AssetImage("assets/jar-loading.gif"),
-        //               image: NetworkImage(
-        //                   "https://gestion.promo.ec/${productsProvider.products[index].imagenes[0]}"),
-        //               fit: BoxFit.cover,
-        //             ),
-        //           ),
-        //         ),
-        //         Padding(
-        //           padding: const EdgeInsets.all(8.0),
-        //           child: Text(
-        //             productsProvider.products[index].nombre,
-        //             style: const TextStyle(
-        //               color: Colors.black45,
-        //             ),
-        //           ),
-        //         ),
-        //         Text(
-        //           ("\$ ${productsProvider.products[index].precio}"),
-        //           style: const TextStyle(
-        //             color: Colors.black87,
-        //             fontSize: 20,
-        //             fontWeight: FontWeight.bold,
-        //           ),
-        //         ),
-        //       ],
-        //     )
-        //   ],
-        // ),
-      
+      // child: Column(
+      //   crossAxisAlignment: CrossAxisAlignment.start,
+      //   children: [
+      //     Column(
+      //       children: [
+      //         Container(
+      //           padding: const EdgeInsets.all(10),
+      //           height: 200,
+      //           width: 180,
+      //           decoration: BoxDecoration(
+      //               color: Colors.amber,
+      //               borderRadius: BorderRadius.circular(15)),
+      //           child: SizedBox(
+      //             child: FadeInImage(
+      //               placeholder: const AssetImage("assets/jar-loading.gif"),
+      //               image: NetworkImage(
+      //                   "https://gestion.promo.ec/${productsProvider.products[index].imagenes[0]}"),
+      //               fit: BoxFit.cover,
+      //             ),
+      //           ),
+      //         ),
+      //         Padding(
+      //           padding: const EdgeInsets.all(8.0),
+      //           child: Text(
+      //             productsProvider.products[index].nombre,
+      //             style: const TextStyle(
+      //               color: Colors.black45,
+      //             ),
+      //           ),
+      //         ),
+      //         Text(
+      //           ("\$ ${productsProvider.products[index].precio}"),
+      //           style: const TextStyle(
+      //             color: Colors.black87,
+      //             fontSize: 20,
+      //             fontWeight: FontWeight.bold,
+      //           ),
+      //         ),
+      //       ],
+      //     )
+      //   ],
+      // ),
     );
   }
 }
